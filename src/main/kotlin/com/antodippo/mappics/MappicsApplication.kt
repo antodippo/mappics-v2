1ec0f769e132165c2269276a5a9bdd806bb30bea
Antonello D'Ippolito <antonello.dippolito@mollie.com>
2024-02-11 15:40:51 +0100

    Kotlin Maven project setup

---
+++ src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
@@ -0,0 +1,11 @@
+package com.antodippo.mappics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MappicsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MappicsApplication>(*args)
+}
---
+++ src/test/kotlin/com/antodippo/mappics/MappicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.antodippo.mappics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MappicsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
