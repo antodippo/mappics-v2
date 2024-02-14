009cb087143e9da842302ed55b1a4a9f197f652e
Antonello D'Ippolito <antonello.dippolito@mollie.com>
2024-02-14 09:55:12 +0100

    Write on a Firestore database

--- src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
+++ src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
@@ -1,5 +1,8 @@
 package com.antodippo.mappics
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.FirestoreOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +11,15 @@ class MappicsApplication
 
 fun main(args: Array<String>) {
 	runApplication<MappicsApplication>(*args)
+
+	val firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
+		.setProjectId("mappics-215209")
+		.setCredentials(GoogleCredentials.getApplicationDefault())
+		.build()
+
+	val db: Firestore = firestoreOptions.service
+
+	db.collection("test")
+		.document("test2")
+		.set(mapOf("name" to "Anto2", "age" to 202))
 }
