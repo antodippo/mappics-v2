8e86a9225ddb4cfe007166cfce60048f1c40edfb
Antonello D'Ippolito <antonello.dippolito@mollie.com>
2024-02-19 08:37:36 +0100

    Test Firestore and Storage

--- src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
+++ src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
@@ -3,6 +3,7 @@ package com.antodippo.mappics
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.firestore.Firestore
 import com.google.cloud.firestore.FirestoreOptions
+import com.google.cloud.storage.StorageOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -20,6 +21,13 @@ fun main(args: Array<String>) {
 	val db: Firestore = firestoreOptions.service
 
 	db.collection("test")
-		.document("test2")
-		.set(mapOf("name" to "Anto2", "age" to 202))
+		.document("test3")
+		.set(mapOf("name" to "Anto3", "age" to 202))
+
+	val storage = StorageOptions.getDefaultInstance().service
+	val bucket = storage.get("mappics-test")
+	bucket.create("uploads/test3.txt", "Hello, World!33333".toByteArray())
+	for (blob in bucket.list().iterateAll()) {
+		println(blob.toString())
+	}
 }
