6a76538cc85330d57d0d34c2fb45024a8f10d3d8
Antonello D'Ippolito <antonello.dippolito@mollie.com>
2024-02-22 19:14:09 +0100

    Revert "Removed Firestore and Storage"
    This reverts commit dfa7bf7fa0b44f235a7abcf402ed9b815bca93a8.

--- src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
+++ src/main/kotlin/com/antodippo/mappics/MappicsApplication.kt
@@ -1,5 +1,9 @@
 package com.antodippo.mappics
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.FirestoreOptions
+import com.google.cloud.storage.StorageOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
